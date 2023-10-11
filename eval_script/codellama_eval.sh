1bb511af1466be1591fd3ac6bae7203cc8417848
zhangir-azerbayev <zazerbayev@gmail.com>
2023-10-11 21:19:46 +0000

    think these are the files I want

--- eval_script/codellama_eval.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source env_setup.sh
-cd ${BASE_DIR}
-
-MODEL=model/codellama_metainstruct_full/checkpoint-9258 
-
-python eval_gsm8k.py --model $MODEL --data_file data/test/GSM8K_test.jsonl --tensor_parallel_size 1
-python eval_math.py --model $MODEL --data_file data/test/MATH_test.jsonl --tensor_parallel_size 1
